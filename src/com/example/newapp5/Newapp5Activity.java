b267daf5a441eda4d79ecb9b10828fb6d602a230
Heart-travel <zcgacbc@gmail.com>
2015-04-19 18:30:26 +0800

    add the action of the two menu

--- src/com/example/newapp5/Newapp5Activity.java
+++ src/com/example/newapp5/Newapp5Activity.java
@@ -38,13 +38,15 @@ public class Newapp5Activity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    	switch (item.getItemId()) {
+    	case R.id.add_item:
+    	Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+    	break;
+    	case R.id.remove_item:
+    	Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+    	break;
+    	default:
+    	}
+    	return true;
+    	}
 }
