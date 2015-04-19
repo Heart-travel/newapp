55a3a3a3a65cbf85aeeb97f0a6bb8e148c3c2b8a
Heart-travel <zcgacbc@gmail.com>
2015-04-19 18:04:51 +0800

    use toast in activity

--- src/com/example/newapp5/Newapp5Activity.java
+++ src/com/example/newapp5/Newapp5Activity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class Newapp5Activity extends Activity {
@@ -13,6 +17,15 @@ public class Newapp5Activity extends Activity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_newapp5);
         setContentView(R.layout.activity_newapp5);
+        
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        	Toast.makeText(Newapp5Activity.this, "You clicked Button 1",
+        	Toast.LENGTH_SHORT).show();
+        	}
+        	});
     }
 
 
