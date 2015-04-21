0297fd219a37aba749a1c4f8f9d2620b86471234
Heart-travel <zcgacbc@gmail.com>
2015-04-21 14:49:05 +0800

    receive data from the next activity

--- src/com/example/newapp5/Newapp5Activity.java
+++ src/com/example/newapp5/Newapp5Activity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,10 +33,13 @@ public class Newapp5Activity extends Activity {
     		intent.setData(Uri.parse("http://www.baidu.com"));
         	startActivity(intent);*/
         	// pass the data to the next activity
-    		String data = "Hello SecondActivity";
+/*    		String data = "Hello SecondActivity";
     		Intent intent = new Intent(Newapp5Activity.this, SecondActivity.class);
     		intent.putExtra("extra_data", data);
-    		startActivity(intent);
+    		startActivity(intent);*/
+    		// receive data from the next activity
+    		Intent intent = new Intent(Newapp5Activity.this, SecondActivity.class);
+    		startActivityForResult(intent, 1);
         	}
         	});
     }
@@ -63,5 +67,18 @@ public class Newapp5Activity extends Activity {
     	default:
     	}
     	return true;
-    	}
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    switch (requestCode) {
+		    case 1:
+		    if (resultCode == RESULT_OK) {
+			    String returnedData = data.getStringExtra("data_return");
+			    Log.d("FirstActivity", returnedData);
+		    }
+		    break;
+	    default:
+	    }
+    }
 }
--- src/com/example/newapp5/SecondActivity.java
+++ src/com/example/newapp5/SecondActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 
 public class SecondActivity extends Activity{
 	
@@ -14,9 +17,21 @@ public class SecondActivity extends Activity{
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.second_layout);
 		// receive the data from the previous activity
-		Intent intent = getIntent();
+/*		Intent intent = getIntent();
 		String data = intent.getStringExtra("extra_data");
-		Log.d("SecondActivity", data);
+		Log.d("SecondActivity", data);*/
+		
+		// send date to the previous activity
+		Button button2 = (Button) findViewById(R.id.button_2);
+		button2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.putExtra("data_return", "Hello FirstActivity");
+				setResult(RESULT_OK, intent);
+				finish();
+			}
+		});
 	}
 
 }
